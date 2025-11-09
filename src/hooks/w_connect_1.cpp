ac1be08c18728f1e36552a62cdcb8e2545bd3db2
svyat <sstndart@gmail.com>
2025-11-10 00:23:41 +0300

    w_connect_1 proper hook

--- src/hooks/w_connect_1.cpp
+++ src/hooks/w_connect_1.cpp
@@ -1,24 +1,46 @@
 #include "target_hooks.h"
 
-void log_w_connect_1() {
+#include <basetsd.h>
+#include <debugapi.h>
+#include <sstream>
+
+typedef UINT_PTR SOCKET;
+
+void __cdecl handle_w_connect_1(SOCKET *sock, LPCWSTR lpString,
+                                uint16_t hostshort) {
   OutputDebugStringA("hook_w_connect_1 fired!");
-  // You can add more complex logic here
+  std::stringstream str;
+  str << "Sock addr " << std::hex << sock << ", lp: " << lpString
+      << ", h: " << hostshort << "\n";
+  OutputDebugStringA(str.str().c_str());
 }
 
 extern "C" void __declspec(naked) hook_w_connect_1() {
-  // Execute original instructions that were overwritten
   __asm {
-        pushad
-        pushfd
-        call log_w_connect_1
-        popfd
-        popad
+    pushad
+    pushfd
 
-        push 0x0FFFFFFFF
-        push 0x00D56220
+        // Push arguments in reverse order (__cdecl convention)
+    mov eax, [esp + 0x28] ; // hostshort (adjust offset based on your stack)
+    push eax
+    mov eax, [esp + 0x2C] ; // lpString  
+    push eax
+    push ecx ; // 'this' pointer (in ECX for __thiscall)
+    
+    call handle_w_connect_1
+
+        // Clean up stack (3 args * 4 bytes = 12)
+    add esp, 12
+
+    popfd
+    popad
+
+        // Execute original instructions that were overwritten
+    push 0x0FFFFFFFF
+    push 0x00D56220
 
     // Jump back to original code after our patch
-        jmp [g_target_w_connect_1.return_rva]
+    jmp [g_target_w_connect_1.return_rva]
   }
 }
 
